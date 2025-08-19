ea41b2b187b05c85399e7b7c0882a370d7e373f5
Garrick Aden-Buie <garrick@adenbuie.com>
2025-08-18 22:24:26 -0400

    🗑️ reset 05-weather_tool

--- 05_weather-tool.R
+++ 05_weather-tool.R
@@ -3,29 +3,21 @@ library(weathR)
 
 woodstock <- list(lat = 34.0951, lon = -84.5179)
 
-weathR::point_forecast(woodstock$lat, woodstock$lon)
+weathR::point_forecast
 
 
 # ---- ⚒️ Let's turn this into a tool 🛠️ ----
 library(ellmer)
 
-ellmer::create_tool_def(weathR::point_forecast, verbose = TRUE)
+ellmer::create_tool_def(_____, verbose = TRUE)
 
-get_weather <- tool(
-  \(lat, lon) weathR::point_forecast(lat, lon),
-  name = "get_weather",
-  description = "Get point meteorological forecast data for a specified latitude and longitude.",
-  arguments = list(
-    lat = type_number("Latitude."),
-    lon = type_number("Longitude.")
-  )
-)
+get_weather <- tool()
 
-get_weather(woodstock$lat, woodstock$lon)
-
+# The tool is callable!
 
 # ---- 🧰 Teach an LLM that we have this tool ----
 chat <- chat_openai(model = "gpt-4.1-nano")
-chat$register_tool(get_weather)
+
+# Register the tool with the chatbot
 
 chat$chat("Is today a good day to go to the pool in Woodstock, GA?")
